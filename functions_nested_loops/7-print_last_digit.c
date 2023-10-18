f6ece1d0c2c1f62bd17fea54cc329367fc9a0999
a7il10 <atilio.sulovari199@gmail.com>
2023-10-18 04:41:18 -0700

    ex 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,16 @@
 #include "main.h"
 
+/**
+ * print_last_digit - it takes an argument of
+ * type int and returns its last digit
+ * @n: integer
+ * Return: the last digit of the number
+ */
+
 int print_last_digit(int n)
 {
 	int last_digit;
-	
+
 	if (n < 0)
 	{
 		n = -n;
