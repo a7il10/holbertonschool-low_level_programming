ec9b576a1defd26679d59fbe05a8a7ba9827451f
a7il10 <atilio.sulovari199@gmail.com>
2023-10-20 02:47:58 -0700

    ex 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,19 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i < 100 && i > 1)
+		if (i <= 100)
 		{
 			printf(" ");
+		}
+		else
+		{
+			printf("\n");
 		}
 		if (i % 3 != 0 && i % 5 != 0)
 		{
 			printf("%d", i);
 		}
-		if (i % 3 == 0)
+		else if (i % 3 == 0)
 		{
 			printf("Fizz");
 		}
