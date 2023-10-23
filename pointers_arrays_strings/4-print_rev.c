cb96c6ba8cb5c3207b4e20bac7cd6a32c6781388
a7il10 <atilio.sulovari199@gmail.com>
2023-10-23 04:39:40 -0700

    ex 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - dhbrjcbn
+ * @s: pointer to a char
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
+	int last_index;
+
+	while (s[len] != '\0')
+	{
+		len++;
+		last_index = len - 1;
+	}
+
+		for (i = last_index; i >= 0; i--)
+			_putchar (s[i]);
+		_putchar ('\n');
+}
