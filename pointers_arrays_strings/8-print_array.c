b19958b6c055887e7b0f9a0c3796c25ba35418f8
a7il10 <atilio.sulovari199@gmail.com>
2023-10-24 03:02:55 -0700

    ex 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - fdvdv
+ * @a: djbfjbgf
+ * @n: dgnrjkgnng
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n - 1; i++)
+	{
+		if (i != n-1)
+			printf("%d ,", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	_putchar ('\n');
+}
