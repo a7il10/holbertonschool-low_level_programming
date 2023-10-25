4e2db4d74d619e9f1723011e91ee77f374782631
a7il10 <atilio.sulovari199@gmail.com>
2023-10-25 00:55:28 -0700

    ex 0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strcat - efbrvbbv
+ * @dest: dfnrnbtj
+ * @src: fbrybu
+ * Return: destination
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+
+	return (dest);
+}
