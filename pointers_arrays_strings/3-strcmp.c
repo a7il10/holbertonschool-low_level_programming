ece9da8c7fdec73db81bf5a5da38edc31b268b30
a7il10 <atilio.sulovari199@gmail.com>
2023-10-25 03:53:49 -0700

    ex 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,24 @@
-include "main.h"
+#include "main.h"
+
+/**
+ * _strcmp - kdsbsv
+ * @s1: dbfgkuhf
+ * @s2: djkfhskhf
+ */
 
 int _strcmp(char *s1, char *s2)
 {
+	int i = 0;
+	int diff = 0;
 
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		i++;
+	}
+	return (diff);
 }
