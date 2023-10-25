cbd46f7834296f37d977e8e866d2d3758ad651de
a7il10 <atilio.sulovari199@gmail.com>
2023-10-25 04:07:54 -0700

    ex 4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
+/**
+ * reverse_array - skdfbsjkb
+ * @a: shbvfjksv
+ * @n: skjrgn
+ */
+
 void reverse_array(int *a, int n)
 {
 	int i, tmp;
 
-	for (i = 0; i < n/2; i++)
+	for (i = 0; i < n / 2; i++)
 	{
 		tmp = a[i];
 		a[i] = a[n - i - 1];
