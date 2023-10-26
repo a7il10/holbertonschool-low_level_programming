b81b232c5b3e37aaf4cdd3fee463dd4bb1ffbb2c
a7il10 <atilio.sulovari199@gmail.com>
2023-10-26 03:27:48 -0700

    ex 1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - djghchjd
+ * @dest: dfdfgkh
+ * @src: dadjfhdjgfhksf
+ * @n: ejhdgfhjrgf
+ * Return: return dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i > n; i++)
+		src[i] = dest[i];
+	return (dest);
+}
