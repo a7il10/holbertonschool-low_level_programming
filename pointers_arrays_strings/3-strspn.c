a888f8d43571421be88381888546dfb67f0fd694
a7il10 <atilio.sulovari199@gmail.com>
2023-10-26 05:33:43 -0700

    ex3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -12,12 +12,13 @@ unsigned int _strspn(char *s, char *accept)
 	int count = 0;
 	int i;
 	int j;
-	int found = 0;
+	int found;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		j = 0;
-		while (s[j] != '\0')
+		found = 0;
+		while (accept[j] != '\0')
 		{
 			if (s[i] == accept[j])
 			{
