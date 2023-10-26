36b0326c9e14b85d19f64ea3eda621ecfd8c518c
a7il10 <atilio.sulovari199@gmail.com>
2023-10-26 04:41:10 -0700

    ex 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - shjfbsbf
+ * @s: sjfbjef
+ * @c: ejfnjeknf
+ * Return: akhfgshbf
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	return NULL;
+}
---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int count = 0;
+	int i;
+	int j;
+	int found = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j = 0;
+		while (s[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				found = 1;
+				count++;
+				break;
+			}
+			j++;
+		}
+		if (found == 0)
+			break;
+	}
+	return (count);
+}
