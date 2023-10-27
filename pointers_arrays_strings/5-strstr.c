17e7de9ac26ae13e60e55454d22077d1086038a1
a7il10 <atilio.sulovari199@gmail.com>
2023-10-27 02:03:05 -0700

    ex 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strstr - jshbfskjdbcsk
+ * @haystack: dsfhbeshkfb
+ * @needle: sacdndsjkbv
+ * Return: needle
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+			if (haystack[i + j] == needle[j])
+			{
+				return (needle);
+			}
+	}
+	return ('\0');
+}
