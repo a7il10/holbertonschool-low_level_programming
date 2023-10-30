0c7eb735058184e588e9e574414a0d216d552401
a7il10 <atilio.sulovari199@gmail.com>
2023-10-30 03:22:56 -0700

    ex 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - cgsdvfhjfd
+ * @s: shgfdhf
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar (s[i]);
+		i++;
+	}
+	_putchar ('\n');
+}
