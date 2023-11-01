191d38b368c7235a7afe3ac5046fe9aae25f608d
a7il10 <atilio.sulovari199@gmail.com>
2023-11-01 06:38:56 -0700

    fix

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,9 +12,9 @@ int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	printf("%d", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
