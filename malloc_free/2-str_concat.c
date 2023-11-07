3b09289728344cedd897c845f9cbba5e853b2c83
a7il10 <atilio.sulovari199@gmail.com>
2023-11-07 02:45:26 -0800

    ex 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,11 +16,11 @@ char *str_concat(char *s1, char *s2)
 	int str_length_2 = 0;
 	int total_length;
 	char *concatenated;
-	
+
 	if (s1 == NULL)
-		return NULL;
+		s1 = "";
 	if (s2 == NULL)
-		return NULL;
+		s2 = "";
 
 	while (s1[str_length_1] != '\0')
 		str_length_1++;
@@ -30,11 +30,11 @@ char *str_concat(char *s1, char *s2)
 	total_length = str_length_1 + str_length_2;
 	concatenated = malloc(sizeof(char) * (total_length + 1));
 	if (concatenated == NULL)
-		return NULL;
+		return (NULL);
 
 	for (i=0; s1[i] != '\0'; i++)
 		concatenated[i] = s1[i];
 	for (i=0; s2[i] != '\0'; i++)
 		concatenated[str_length_1 + i] = s2[i];
-	return concatenated;
+	return (concatenated);
 }
