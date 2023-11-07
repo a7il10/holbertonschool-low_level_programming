9ccf934d88aafd92d2ec58c87a0394868e9ed047
a7il10 <atilio.sulovari199@gmail.com>
2023-11-07 02:39:11 -0800

    ex 2 fix 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,32 +11,30 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *concate;
-	int str_len1 = 0;
-	int str_len2 = 0;
-	int total_length;
 	int i;
-
+	int str_length_1 = 0;
+	int str_length_2 = 0;
+	int total_length;
+	char *concatenated;
+	
 	if (s1 == NULL)
-		return (NULL);
+		return NULL;
 	if (s2 == NULL)
-		return (NULL);
+		return NULL;
 
-	while (s1[str_len1] != '\0')
-		str_len1++;
+	while (s1[str_length_1] != '\0')
+		str_length_1++;
+	while (s2[str_length_2] != '\0')
+		str_length_2++;
 
-	while (s2[str_len2] != '\0')
-		str_len2++;
+	total_length = str_length_1 + str_length_2;
+	concatenated = malloc(sizeof(char) * (total_length + 1));
+	if (concatenated == NULL)
+		return NULL;
 
-	total_length = str_len1 + str_len2;
-	concate = malloc(sizeof(char) * (total_length + 1));
-	if (concate == NULL)
-		return (NULL);
-
-	for (i = 0; s1[i] != '\0'; i++)
-		concate[i] = s1[i];
-
-	for (i = 0; s2[i] != '\0'; i++)
-		concate[str_len1 + i] = s2[i];
-	return (concate);
+	for (i=0; s1[i] != '\0'; i++)
+		concatenated[i] = s1[i];
+	for (i=0; s2[i] != '\0'; i++)
+		concatenated[str_length_1 + i] = s2[i];
+	return concatenated;
 }
