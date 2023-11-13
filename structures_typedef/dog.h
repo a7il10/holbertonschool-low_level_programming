f8dbb942b5493cdf2656006cfc6cd413979857c9
a7il10 <atilio.sulovari199@gmail.com>
2023-11-13 02:41:20 -0800

    ex 1 update

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - name of datatype
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner name of the dog
+ *
+ * Description: Define a new type struct dog with the following elements:
+ */
+
 struct dog
 {
 	char *name;
