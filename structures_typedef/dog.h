f1aff0ba14730ad82b2a333001629634cd7d6dff
a7il10 <atilio.sulovari199@gmail.com>
2023-11-13 04:39:16 -0800

    ex 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -10,12 +10,12 @@
  * Description: Define a new type struct dog with the following elements:
  */
 
-struct dog
+typedef dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
