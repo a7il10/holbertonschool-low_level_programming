0f663296ba353b1e71d04b799e0c68d75fce6ad6
a7il10 <atilio.sulovari199@gmail.com>
2023-11-15 02:31:49 -0800

    ex 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * array_iterator - iterate through the array
+ * @array: array of int
+ * @size: size of the array
+ * @action: function pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
