8b60a30e304d9a4e41e29c647c52ecfa6f10e5f4
a7il10 <atilio.sulovari199@gmail.com>
2023-12-01 03:12:46 -0800

    ex3: fic karnel style

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,7 +13,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 	dlistint_t *tmp = *head;
-	
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
