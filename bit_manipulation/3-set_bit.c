5d8652b71dfb348be0a12bfb82dbfbe9126ab6cc
a7il10 <atilio.sulovari199@gmail.com>
2023-12-02 08:44:53 -0800

    fix karnel style

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -4,7 +4,7 @@
 /**
  * set_bit - sets the value of a bit to 1 at a given index
  * @n: pointer to the value to be changed
- * @incdex: index of the bit to be changed
+ * @index: index of the bit to be changed
  * Return: 1 if it worked or -1 if an error ocurred
  */
 
