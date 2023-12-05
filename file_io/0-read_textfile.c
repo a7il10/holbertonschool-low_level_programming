478bcb8b1c87d312b62365123a17cfe44e3ea9ab
a7il10 <atilio.sulovari199@gmail.com>
2023-12-05 02:45:03 -0800

    ex 0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it
+ * @filename: readed file
+ * @letters: number of letters should be printed
+ * Return: number of letters it could read and print, 0 if filename is NULL, 0 if write fails or does not write the expected amount of bytes
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, readed, printed;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	readed = read(fd, buffer, letters);
+	if (readed == -1)
+		return (0);
+	printed = write(STDOUT_FILENO, buffer, readed);
+	if (printed == -1)
+		return (0);
+	close(fd);
+	free(buffer);
+	return (printed);
+}
