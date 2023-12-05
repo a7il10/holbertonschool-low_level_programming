a2b748b8f756180355b5e50f83e6802fafbbf839
a7il10 <atilio.sulovari199@gmail.com>
2023-12-05 09:37:47 -0800

    ex 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		by_wr = write(fd, text_content, strlen(text_content));
 
-	if(by_wr == -1)
+	if (by_wr == -1)
 	{
 		close(fd);
 	return (-1);
