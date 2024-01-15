bae881af151f5f4f6987287b1dba0254e8103069
a7il10 <atilio.sulovari199@gmail.com>
2024-01-15 12:44:59 +0100

    fix betty

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,5 +1,4 @@
 #include "search_algos.h"
-#include <stdio.h>
 
 /**
  * linear_search - implements the linear search algorithm.
@@ -12,22 +11,20 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-
-size_t i;
-
+	size_t i;
+	
 	if (array == NULL)
 	{
-	return (-1);
+		return (-1);
 	}
-
+	
 	for (i = 0; i < size; i++)
 	{
-
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
 			return (i);
 		}
 	}
-		return (-1);
+	return (-1);
 }
