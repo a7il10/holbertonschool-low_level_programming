92f151c92ca22ab58ec6f7c26d79efb0bf8aabb0
a7il10 <atilio.sulovari199@gmail.com>
2024-01-15 12:36:15 +0100

    fix betty

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -6,15 +6,17 @@
  * @size: number of elements in the array.
  * @value: value to search for.
  * Return: The first index where value is located
- */
- 
+*/
+
 int linear_search(int *array, size_t size, int value)
 {
 
 size_t i;
 
 	if (array == NULL)
-	return -1;
+	{
+	return (-1);
+	}
 
 	for (i = 0; i < size; i++)
 	{
@@ -22,8 +24,8 @@ size_t i;
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
-			return i;
+			return (i);
 		}
 	}
-		return -1;
+		return (-1);
 }
